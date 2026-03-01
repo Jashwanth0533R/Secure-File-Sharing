76ea734291072541ef88399b1a2862842c05b8f5
Jashwanth G <gjashwanthkumar711@gmail.com>
2026-03-01 17:44:58 +0530

    Add frontend for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -6,3 +6,33 @@ const container = document.getElementById("liquid-bg");
 new AnimatedLiquidBackground({
     target: container
 });
+async function handleSignin() {
+
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+        window.location.href = "login/login.html";
+        return;
+    }
+
+    try {
+        const res = await fetch(
+            "https://secure-file-sharing-4k5x.onrender.com/profile",
+            {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            }
+        );
+
+        if (res.ok) {
+            window.location.href = "user_data.html";
+        } else {
+            localStorage.removeItem("token");
+            window.location.href = "login/login.html";
+        }
+
+    } catch {
+        window.location.href = "login/login.html";
+    }
+}
